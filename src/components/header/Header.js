ad1f461ebc2d9946e529646f21e8d48cb3260eaf
Hesham749 <heesham749@gmail.com>
2024-04-04 15:29:20 +0200

    finish-navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -36,43 +36,39 @@ export default function Header() {
     },
   ];
   const [isOpen, setIsOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("Home");
 
   return (
     <>
-      <Navbar className="fixed bottom-0 md:sticky md:top-0 w-full px-7 md:px-0 bg-body-color ">
+      <Navbar className="fixed bottom-0 md:sticky md:top-0 w-full px-7 md:px-0  bg-body-color ">
         <Navbar.Brand href="#">
           <span className="self-center whitespace-nowrap text-xl font-semibold text-gray-700">
             Hesham
           </span>
         </Navbar.Brand>
-        <div className="flex  md:order-2">
-          <div
-            onClick={() => setIsOpen(!isOpen)}
-            className="relative text-center ">
-            {isOpen ? (
-              <i className="bx bx-x text-2xl md:hidden  "></i>
-            ) : (
-              <i className="bx bx-grid-alt text-xl md:hidden"></i>
-            )}
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex  md:order-2 md:hidden relative text-center overflow-hidden ">
+          {isOpen ? (
+            <i className="bx bx-x text-2xl md:hidden  "></i>
+          ) : (
+            <i className="bx bx-grid-alt text-xl md:hidden"></i>
+          )}
 
-            <Navbar.Toggle className="absolute left-0 max-w-full top-0 opacity-0 " />
-          </div>
+          <Navbar.Toggle className="absolute left-0 w-full max-w-full h-full top-0 opacity-0 " />
         </div>
         <Navbar.Collapse className="absolute bottom-[100%]  left-0 md:static">
           <div className=" font-medium grid  grid-cols-3 gap-3 p-4 md:flex md:p-0 mt-4 border bg-gray-50 md:bg-transparent border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
             {links.map((link, i) => (
               <Navbar.Link
-                onClick={(e) => {
-                  [...document.querySelectorAll("a.active")].map((a) =>
-                    a.classList.remove("active")
-                  );
-                  e.target.classList.add("active");
-                  setIsOpen(false)
+                onClick={() => {
+                  setActiveLink(link.name);
+                  setIsOpen(false);
                 }}
                 key={i}
                 href="#"
                 className={`${
-                  i === 0 && "active"
+                  activeLink === link.name && "active"
                 } flex flex-col items-center font-bold py-2 px-3 rounded border-none text-gray-500 hover:text-title-color-dark  `}>
                 <i className={`bx bx-${link.icon} md:hidden `}></i>
                 {link.name}
