6a7a57a6fe0119c80de62c4b9835e022d8d96f83
Hesham749 <heesham749@gmail.com>
2024-04-05 21:25:34 +0200

    update-social links

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Social() {
   return (
     <div className='flex flex-col w-fit text-xl text-gray-500 '>
-      <a href="#" target="_blank" rel="noopener noreferrer"><i className='bx bxl-github  hover:text-title-color-dark'></i></a>
-      <a href="#" target="_blank" rel="noopener noreferrer"><i className='bx bxl-linkedin-square hover:text-title-color-dark' ></i></a>
-      <a href="#" target="_blank" rel="noopener noreferrer"><i className='bx bxl-instagram-alt hover:text-title-color-dark' ></i></a>
+      <a href="https://github.com/Hesham749" target="_blank" rel="noopener noreferrer"><i className='bx bxl-github  hover:text-title-color-dark'></i></a>
+      <a href="https://www.linkedin.com/in/hesham-elsayed-5034ba179/" target="_blank" rel="noopener noreferrer"><i className='bx bxl-linkedin-square hover:text-title-color-dark' ></i></a>
+      <a href="https://www.instagram.com/hesham_elsayeed/" target="_blank" rel="noopener noreferrer"><i className='bx bxl-instagram-alt hover:text-title-color-dark' ></i></a>
     </div>
   )
 }
