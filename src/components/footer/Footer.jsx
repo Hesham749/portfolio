9e809db73b819e3c8a2cd05d356fc329c6b220fd
Hesham749 <heesham749@gmail.com>
2024-04-06 20:12:35 +0200

    u

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,24 +7,6 @@ export default function FOoter() {
     <footer className="bg-white text-center mb-14 md:mb-0 py-5 border-t border-gray-200">
       <div className="md:w-fit md:mx-auto justify-self-center flex flex-col gap-5 justify-center items-center ">
         <h3 className="text-title-color-dark text-3xl font-bold">Hesham</h3>
-        {/* links */}
-        <div className="flex gap-10 justify-center ">
-          <a
-            href="#about"
-            className="text-title-color hover:text-title-color-dark font-semibold">
-            About
-          </a>
-          <a
-            href="#projects"
-            className="text-title-color hover:text-title-color-dark font-semibold">
-            Projects
-          </a>
-          <a
-            href="#testimonials"
-            className="text-title-color hover:text-title-color-dark font-semibold">
-            Testimonials
-          </a>
-        </div>
         {/* icons */}
         <div className="flex gap-5 justify-center">
           <a
@@ -43,9 +25,31 @@ export default function FOoter() {
             <i className="fa-brands fa-square-instagram"></i>
           </a>
         </div>
+        {/* links */}
+        <div className="flex gap-5 justify-center ">
+          <a
+            href="#about"
+            className="text-title-color hover:text-title-color-dark font-semibold">
+            About
+          </a>
+          <a
+            href="#projects"
+            className="text-title-color hover:text-title-color-dark font-semibold">
+            Projects
+          </a>
+          <a
+            href="#testimonials"
+            className="text-title-color hover:text-title-color-dark font-semibold">
+            Testimonials
+          </a>
+        </div>
+
         {/* copyright */}
         <p className="text-title-color text-sm">
-          Copyright © {year} Hesham. All rights reserved.
+          <span className="text-title-color-dark font-semibold">
+            Copyright ©
+          </span>{" "}
+          {year} Hesham. All rights reserved.
         </p>
       </div>
     </footer>
