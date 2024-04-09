9dde98e58c27c5e7fd9d4ccc50ac1b188e105d94
Hesham749 <heesham749@gmail.com>
2024-04-10 00:12:18 +0200

    u

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.min.css';
-import App from './App';
-import 'flowbite';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.min.css";
+import App from "./App";
+import "flowbite";
 
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-
     <App />
-
   </React.StrictMode>
 );
