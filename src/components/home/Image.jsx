c7a7397f08e5f5f64e39f92ac3e74549a29abff3
Hesham749 <heesham749@gmail.com>
2024-04-10 02:37:51 +0200

    u

--- src/components/home/Image.jsx
+++ src/components/home/Image.jsx
@@ -4,6 +4,7 @@ export default function Image() {
   return (
     <div className="w-60 h-60 md:w-80 md:h-80 md:order-1   ">
       <img
+      loading="lazy"
         src={img}
         alt=""
         className="image w-full h-full  border-8 border-gray-300 "
