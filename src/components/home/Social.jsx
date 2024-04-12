3c1a5f114541eaec125680f366ce2d900d3d2223
Hesham749 <heesham749@gmail.com>
2024-04-12 18:56:32 +0200

    u

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 export default function Social() {
-  return (
-    <div className="flex flex-col w-fit text-xl text-gray-500 ">
-      <a
-        href="https://github.com/Hesham749"
-        target="_blank"
-        rel="noopener noreferrer">
-        <i className="bx bxl-github  text-xl hover:text-title-color-dark"></i>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/hesham-elsayed-5034ba179/"
-        target="_blank"
-        rel="noopener noreferrer">
-        <i className="bx bxl-linkedin-square text-xl hover:text-title-color-dark"></i>
-      </a>
-      <a
-        href="https://www.instagram.com/hesham_elsayeed/"
-        target="_blank"
-        rel="noopener noreferrer">
-        <i className="bx bxl-instagram-alt text-xl hover:text-title-color-dark"></i>
-      </a>
-    </div>
-  );
+    return (
+        <div className="flex flex-col w-fit text-xl text-gray-500 ">
+            <a
+                href="https://github.com/Hesham749"
+                target="_blank"
+                rel="noopener noreferrer">
+                <i className="bx bxl-github  text-xl hover:text-title-color-dark"></i>
+            </a>
+            <a
+                href="https://www.linkedin.com/in/hesham-elsayed-5034ba179/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <i className="bx bxl-linkedin-square text-xl hover:text-title-color-dark"></i>
+            </a>
+            <a
+                href="https://www.instagram.com/hesham_elsayeed/"
+                target="_blank"
+                rel="noopener noreferrer">
+                <i className="bx bxl-instagram-alt text-xl hover:text-title-color-dark"></i>
+            </a>
+        </div>
+    );
 }
