ac0a851929870f5420239c3344b0dce98a4fcc72
Hesham749 <heesham749@gmail.com>
2024-04-14 15:43:32 +0200

    u

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -11,7 +11,7 @@ export default function Info() {
       <div className="bg-white py-3 px-5 md:px-0 border-[1px] border-gray-200 rounded-xl flex flex-col items-center justify-start">
         <i className="bx bx-briefcase-alt text-2xl"></i>
         <h3 className="font-semibold">Completed</h3>
-        <span className=" text-title-color text-sm mt-1">5 + Projects</span>
+        <span className=" text-title-color text-sm mt-1">10 + Projects</span>
       </div>
       <div className="bg-white py-3 px-5 md:px-0 border-[1px] border-gray-200 rounded-xl flex flex-col items-center justify-start">
         <i className="bx bx-headphone text-2xl"></i>
